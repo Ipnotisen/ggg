da9d4b868437ca6a07f82fea50e298946a0d7002
GodIpno <attilaboys@gmail.com>
2023-01-14 23:51:44 +0100

    dark mode

--- app.js
+++ app.js
@@ -11,3 +11,20 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('click', () => {
+    const body = document.body;
+    if(body.classList.contains('dark')){
+
+        body.classList.add('light')
+        body.classList.remove('dark')
+
+    } else if(body.classList.contains('light')){
+
+        body.classList.add('dark')
+        body.classList.remove('light')
+
+    }
+})
