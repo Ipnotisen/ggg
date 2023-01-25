d9942386e248ebc7a07892c194c9eac91d00f829
GodIpno <attilaboys@gmail.com>
2023-01-25 05:48:21 +0100

    dsadasd

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+
+// Loader
+const loader = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+    document.body.style.overflow = "hidden";
+    setTimeout(() => {
+        loader.classList.add('fondu-out');
+        document.body.style.overflow = "visible";
+    }, 1200);
+});
+
 // Animation on scroll
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
         }
     });
 });
